cee8af33821ee43ce3dbc1918324dbf4203341f5
Daniel La <la.danieln@gmail.com>
2018-11-16 00:26:53 -0500

    Contact submission form controller

---
+++ controllers/contact-submission.php
@@ -0,0 +1,19 @@
+<?php 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/models/contact.php';
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$phoneNumber = $_POST['phoneNumber'];
+$message = $_POST['message'];
+
+$contactSubmission = new Contact($firstName, $lastName, $email, $phoneNumber, $message);
+
+$result = $contactSubmission->insertNewContactSubmission();
+
+if ($result){
+    echo json_encode(array('response' => true));
+} else {
+    echo json_encode(array('response' => false));
+}
+?>
