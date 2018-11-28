fb1cf78e2acb742bae7e2793e05c736e2c49b2d8
Daniel La <la.danieln@gmail.com>
2018-11-27 20:19:13 -0500

    Active page check added

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -1,3 +1,19 @@
+<?php 
+$requestUri = $_SERVER['REQUEST_URI'];
+
+$requestedPath = explode('/', $requestUri);
+$classActive = '<li class="active">';
+$notActive = '<li>';
+if (!empty($requestedPath[2])){
+    if (strpos($requestedPath[2], '.php') !== false){
+        $positionOfDotPhp = strpos($requestedPath[2], '.php');
+
+        $currentPage = ucwords(substr($requestedPath[2], 0, $positionOfDotPhp));
+    }
+}
+
+?>
+
 <!-- begin #sidebar -->
 <div id="sidebar" class="sidebar">
     <!-- begin sidebar scrollbar -->
@@ -26,32 +42,43 @@
         <!-- begin sidebar nav -->
         <ul class="nav">
             <li class="nav-header">Navigation</li>
-            <li class="active">
+            <?php 
+            if ($currentPage == 'Dashboard'){
+                echo $classActive;
+            } else {
+                echo $notActive;
+            }
+            ?>
                 <a href="dashboard.php">
                     <b class="caret"></b>
                     <i class="fa fa-th-large"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
-            <!-- <li class="has-sub">
-                <a href="javascript:;">
-                    <span class="badge pull-right">10</span>
-                    <i class="fa fa-hdd"></i>
-                    <span>Email</span>
-                </a>
-                <ul class="sub-menu">
-                    <li><a href="email_inbox.html">Inbox</a></li>
-                    <li><a href="email_compose.html">Compose</a></li>
-                    <li><a href="email_detail.html">Detail</a></li>
-                </ul>
-            </li> -->
-            <li>
+            <?php 
+            if ($currentPage == 'Contacts'){
+                echo $classActive;
+            } else {
+                echo $notActive;
+            }
+            ?>
                 <a href="contacts.php">
                     <i class="fab fa-simplybuilt"></i>
                     <span>Contact Submissions</span>
                 </a>
             </li>
-            <li><a href="events.php"><i class="fa fa-calendar"></i> <span>Calendar</span></a></li>
+            <?php 
+            if ($currentPage == 'Events'){
+                echo $classActive;
+            } else {
+                echo $notActive;
+            }
+            ?>
+                <a href="events.php">
+                    <i class="fa fa-calendar"></i> 
+                    <span>Events</span>
+                </a>
+            </li>
             <!-- begin sidebar minify button -->
             <li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
             <!-- end sidebar minify button -->
