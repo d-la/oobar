3442413932d2a4a4b53ba5771fed00f13f51371b
Daniel La <la.danieln@gmail.com>
2018-11-27 21:20:38 -0500

    Replaced stored procedures with dynamic SQL queries

--- controllers/add-event.php
+++ controllers/add-event.php
@@ -8,7 +8,36 @@ $eventDate = date("Y-m-d", strtotime($_POST['eventDate']));
 $eventStartTime = date('H:i:s', strtotime($_POST['startTime']));
 $eventEndTime = date('H:i:s', strtotime($_POST['endTime']));
 
-$sqlQuery = "CALL spInsertNewEvent('" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . $eventDate . "', '" . $eventStartTime . "', '" . $eventEndTime . "');";
+$fileName = $_FILES['bannerImage']['name'];
+$fileType = $_FILES['bannerImage']['type'];
+$fileSize = $_FILES['bannerImage']['size'];
+// Temporary Location
+$fileTmp = $_FILES['bannerImage']['tmp_name'];
+$fileError = $_FILES['bannerImage']['error'];
+
+$directoryToUpload = $_SERVER['DOCUMENT_ROOT'] . '/img/events/banners/' . $eventId . '/';
+$directoryToUploadWithFileName = $directoryToUpload . $fileName;
+
+$registrationLink = $_POST['registrationLink'];
+
+$url = $mysqli->real_escape_string($registrationLink);
+
+if ($fileError === 0){
+    die();
+} else {
+    // Perform upload
+
+    if (!is_dir($directoryToUpload)){
+        mkdir($directoryToUpload, 0777, true);
+    }
+    move_uploaded_file($fileTmp, $directoryToUploadWithFileName);
+}
+
+// $sqlQuery = "CALL spInsertNewEvent('" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . $eventDate . "', '" . $eventStartTime . "', '" . $eventEndTime . "');";
+
+$sqlQuery = "INSERT INTO events (event_name, event_desc, event_date, start_time, end_time, created_date, banner_path, registration_url)";
+$sqlQuery .= "VALUES ('" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . addslashes($eventDate) . "', '" . addslashes($eventStartTime) . "', '" . addslashes($eventEndTime) . "', NOW()), '" . addslashes($directoryToUploadWithFileName) . "', '" . $registrationLink . "';";
+
 
 $result = $mysqli->query($sqlQuery);
 
--- controllers/update-event.php
+++ controllers/update-event.php
@@ -3,23 +3,45 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/include/mysqlconn.php';
 $mysqli = initializeMysqlConnection();
 
 $eventId = filter_var($_POST['eventId'], FILTER_VALIDATE_INT);
-$eventTitle = $_POST['eventName'];
-$eventDesc = $_POST['eventDesc'];
+$eventTitle = addslashes($_POST['eventName']);
+$eventDesc = addslashes($_POST['eventDesc']);
 $eventDate = date("Y-m-d", strtotime($_POST['eventDate']));
 $eventStartTime = date('H:i:s', strtotime($_POST['startTime']));
 $eventEndTime = date('H:i:s', strtotime($_POST['endTime']));
 
-$sqlQuery = "CALL spUpdateEventDetails($eventId, '" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . $eventDate . "', '" . $eventStartTime . "', '" . $eventEndTime . "');";
+$directoryToUpload = $_SERVER['DOCUMENT_ROOT'] . '/img/events/banners/' . $eventId . '/';
+$directoryToUploadWithFileName = $directoryToUpload . $fileName;
 
-$result = $mysqli->query($sqlQuery);
+$registrationLink = $_POST['registrationLink'];
 
-if ($result == true){
-    header('Location: /admin/events.php');
+$url = $mysqli->real_escape_string($registrationLink);
+
+if ($fileError === 0){
     die();
 } else {
-    header('Location: /admin/events.php');
-    die();
+    // Perform upload
+
+    if (!is_dir($directoryToUpload)){
+        mkdir($directoryToUpload, 0777, true);
+    }
+    move_uploaded_file($fileTmp, $directoryToUploadWithFileName);
 }
+
+// $sqlQuery = "CALL spUpdateEventDetails($eventId, '" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . $eventDate . "', '" . $eventStartTime . "', '" . $eventEndTime . "');";
+
+$directoryToUploadWithFileName = addslashes($directoryToUploadWithFileName);
+$sqlQuery = "UPDATE events SET event_name = '$eventTitle', event_desc = '$eventDesc', event_date = '$eventDate', start_time = '$eventStartTime', end_time = '$eventEndTime', updated_date = NOW(), registration_url = '$registrationLink', banner_path = '$directoryToUploadWithFileName'";
+$sqlQuery .= " WHERE id = $eventId;";
+echo $sqlQuery;
+// $result = $mysqli->query($sqlQuery);
+
+// if ($result == true){
+//     header('Location: /admin/events.php');
+//     die();
+// } else {
+//     header('Location: /admin/events.php');
+//     die();
+// }
 
 
 ?>
