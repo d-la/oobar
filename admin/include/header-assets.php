930e8e20bdb2bdedcbb13688b2279009fa16706d
Daniel La <la.danieln@gmail.com>
2018-11-27 20:06:09 -0500

    Common html placed in one file to be required in main files

---
+++ admin/include/header-assets.php
@@ -0,0 +1,25 @@
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+<meta content="" name="description" />
+<meta content="" name="author" />
+
+<!-- ================== BEGIN BASE CSS STYLE ================== -->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/font-awesome/5.3/css/all.min.css" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/animate/animate.min.css" rel="stylesheet" />
+<link href="/admin/admin_assets/css/default/style.min.css" rel="stylesheet" />
+<link href="/admin/admin_assets/css/default/style-responsive.min.css" rel="stylesheet" />
+<link href="/admin/admin_assets/css/default/theme/default.css" rel="stylesheet" id="theme" />
+<!-- ================== END BASE CSS STYLE ================== -->
+
+<!-- ================== BEGIN PAGE LEVEL CSS STYLE ================== -->
+<link href="/admin/admin_assets/plugins/jquery-jvectormap/jquery-jvectormap.css" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/bootstrap-calendar/css/bootstrap_calendar.css" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/gritter/css/jquery.gritter.css" rel="stylesheet" />
+<link href="/admin/admin_assets/plugins/nvd3/build/nv.d3.css" rel="stylesheet" />
+<!-- ================== END PAGE LEVEL CSS STYLE ================== -->
+
+<!-- ================== BEGIN BASE JS ================== -->
+<script src="/admin/admin_assets/plugins/pace/pace.min.js"></script>
+<!-- ================== END BASE JS ================== -->
---
+++ admin/include/sidebar.php
@@ -0,0 +1,64 @@
+<!-- begin #sidebar -->
+<div id="sidebar" class="sidebar">
+    <!-- begin sidebar scrollbar -->
+    <div data-scrollbar="true" data-height="100%">
+        <!-- begin sidebar user -->
+        <ul class="nav">
+            <li class="nav-profile">
+                <a href="javascript:;" data-toggle="nav-profile">
+                    <div class="cover with-shadow"></div>
+                    <div class="image">
+                        <img src="/admin/admin_assets/img/user/user-13.jpg" alt="" />
+                    </div>
+                    <div class="info">
+                        <b class="caret pull-right"></b>
+                        Oobar Admin
+                    </div>
+                </a>
+            </li>
+            <li>
+                <ul class="nav nav-profile">
+                    <li><a href="javascript:;"><i class="fa fa-cog"></i> Settings</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- end sidebar user -->
+        <!-- begin sidebar nav -->
+        <ul class="nav">
+            <li class="nav-header">Navigation</li>
+            <li class="active">
+                <a href="dashboard.php">
+                    <b class="caret"></b>
+                    <i class="fa fa-th-large"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <!-- <li class="has-sub">
+                <a href="javascript:;">
+                    <span class="badge pull-right">10</span>
+                    <i class="fa fa-hdd"></i>
+                    <span>Email</span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="email_inbox.html">Inbox</a></li>
+                    <li><a href="email_compose.html">Compose</a></li>
+                    <li><a href="email_detail.html">Detail</a></li>
+                </ul>
+            </li> -->
+            <li>
+                <a href="contacts.php">
+                    <i class="fab fa-simplybuilt"></i>
+                    <span>Contact Submissions</span>
+                </a>
+            </li>
+            <li><a href="events.php"><i class="fa fa-calendar"></i> <span>Calendar</span></a></li>
+            <!-- begin sidebar minify button -->
+            <li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
+            <!-- end sidebar minify button -->
+        </ul>
+        <!-- end sidebar nav -->
+    </div>
+    <!-- end sidebar scrollbar -->
+</div>
+<div class="sidebar-bg"></div>
+<!-- end #sidebar -->
