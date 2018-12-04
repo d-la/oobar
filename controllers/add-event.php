c3140817a0d2425b54b13356edfbe7660e7182f0
Daniel La <la.danieln@gmail.com>
2018-12-03 22:39:41 -0500

    Logic fixed to allow for banner uploads

--- controllers/add-event.php
+++ controllers/add-event.php
@@ -15,39 +15,76 @@ $fileSize = $_FILES['bannerImage']['size'];
 // Temporary Location
 $fileTmp = $_FILES['bannerImage']['tmp_name'];
 $fileError = $_FILES['bannerImage']['error'];
-
-$directoryToUpload = $_SERVER['DOCUMENT_ROOT'] . '/img/events/banners/' . $eventId . '/';
-$directoryToUploadWithFileName = $directoryToUpload . $fileName;
-
 $registrationLink = $_POST['registrationLink'];
+
+// Get the file extension
+$fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+// Generate random file name since we're storing on our server and not an external instance
+$randomFileName = 'event_banner_' . rand(0, 10000) . $fileExtension;
+
+// Full directory to upload
+$directoryToUpload = $_SERVER['DOCUMENT_ROOT'] . '/img/events/banners/';
+
+// Path that will pull the file on front end
+$basePathForFrontEnd = '/img/events/banners/';
+
+
+$pathToBeSaved = $basePathForFrontEnd . $randomFileName;
+
+$directoryToUploadWithFileName = $directoryToUpload . $randomFileName;
 
 $url = $mysqli->real_escape_string($registrationLink);
 
-if ($fileError === 0){
-    die();
+if (file_exists($directoryToUploadWithFileName)){
+    
+    $uploadPath = returnRandomFileName($directoryToUpload, $fileExtension);
+    $fileStatus = move_uploaded_file($fileTmp, $uploadPath);
+
+    $pathToBeSaved = $basePathForFrontEnd . basename($uploadPath);
+
+    if ($fileStatus){
+        $_SESSION['alert'] = 'file success';
+    } else {
+        $_SESSION['alert'] = 'file error';
+    }
 } else {
     // Perform upload
+    $fileStatus = move_uploaded_file($fileTmp, $directoryToUploadWithFileName);
 
-    if (!is_dir($directoryToUpload)){
-        mkdir($directoryToUpload, 0777, true);
+    if ($fileStatus){
+        $_SESSION['alert'] = 'file success';
+    } else {
+        $_SESSION['alert'] = 'file error';
     }
-    move_uploaded_file($fileTmp, $directoryToUploadWithFileName);
 }
 
 // $sqlQuery = "CALL spInsertNewEvent('" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . $eventDate . "', '" . $eventStartTime . "', '" . $eventEndTime . "');";
 
 $sqlQuery = "INSERT INTO events (event_name, event_desc, event_date, start_time, end_time, created_date, banner_path, registration_url)";
-$sqlQuery .= "VALUES ('" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . addslashes($eventDate) . "', '" . addslashes($eventStartTime) . "', '" . addslashes($eventEndTime) . "', NOW()), '" . addslashes($directoryToUploadWithFileName) . "', '" . $registrationLink . "';";
-
+$sqlQuery .= "VALUES ('" . addslashes($eventTitle) . "', '" . addslashes($eventDesc) . "', '" . addslashes($eventDate) . "', '" . addslashes($eventStartTime) . "', '" . addslashes($eventEndTime) . "', NOW(), '" . addslashes($pathToBeSaved) . "', '" . $registrationLink . "');";
 
 $result = $mysqli->query($sqlQuery);
 
 if ($result == true){
+    $_SESSION['alert'] = 'success';
     header('Location: /admin/events.php');
     die();
 } else {
+    $_SESSION['alert'] = 'error';
     header('Location: /admin/events.php');
     die();
+}
+
+function returnRandomFileName($directoryToUpload, $fileExtension){
+    $randomFileName = 'event_banner_' . rand(0, 10000) . $fileExtension;
+    $newUploadPath = $directoryToUpload . $randomFileName;
+
+    if (file_exists($newUploadPath)){
+        return returnRandomFileName($directoryToUpload);
+    } else {
+        return $newUploadPath;
+    }
 }
 
 
