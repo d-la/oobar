a41a8e69501d2f2c6c737f31ce58def32b47f8ec
Daniel La <la.danieln@gmail.com>
2018-12-04 23:33:22 -0500

    Boilerplate gallery added to home page

--- index.php
+++ index.php
@@ -72,6 +72,7 @@ $mysqli = initializeMysqlConnection();
                         </li> -->
                         <li class="nav-item"><a class="nav-link hover-theme-color" href="#about" data-click="scroll-to-target">ABOUT</a></li>
                         <li class="nav-item"><a class="nav-link hover-theme-color" href="#events" data-click="scroll-to-target">EVENTS</a></li>
+                        <li class="nav-item"><a class="nav-link hover-theme-color" href="#gallery" data-click="scroll-to-target">GALLERY</a></li>
                         <li class="nav-item"><a class="nav-link hover-theme-color" href="#contact" data-click="scroll-to-target">CONTACT</a></li>
                     </ul>
                 </div>
@@ -228,6 +229,146 @@ $mysqli = initializeMysqlConnection();
         </div>
         <!-- end #events -->
 
+        <!-- begin #gallery -->
+		<div id="gallery" class="content" data-scrollview="true">
+			<!-- begin container -->
+			<div class="container" data-animation="true" data-animation-type="fadeInDown">
+				<h2 class="content-title">Gallery</h2>
+				<p class="content-desc">
+				</p>
+				<!-- begin row -->
+				<div class="row row-space-10">
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-1.jpg" alt="Work 1" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Aliquam molestie</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-2.jpg" alt="Work 2" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Quisque at pulvinar lacus</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-3.jpg" alt="Work 3" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Vestibulum et erat ornare</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-4.jpg" alt="Work 4" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Sed vitae mollis magna</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+				</div>
+				<!-- end row -->
+				<!-- begin row -->
+				<div class="row row-space-10">
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-5.jpg" alt="Work 5" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Suspendisse at mattis odio</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-6.jpg" alt="Work 6" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Aliquam vitae commodo diam</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-7.jpg" alt="Work 7" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Phasellus eu vehicula lorem</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+					<!-- begin col-3 -->
+					<div class="col-md-3 col-sm-6">
+						<!-- begin work -->
+						<div class="work">
+							<div class="image">
+								<a href="#"><img src="../assets/img/work/work-img-8.jpg" alt="Work 8" /></a>
+							</div>
+							<div class="desc">
+								<span class="desc-title">Morbi bibendum pellentesque</span>
+								<span class="desc-text">Lorem ipsum dolor sit amet</span>
+							</div>
+						</div>
+						<!-- end work -->
+					</div>
+					<!-- end col-3 -->
+				</div>
+				<!-- end row -->
+			</div>
+			<!-- end container -->
+		</div>
+		<!-- end #gallery -->
+
         <!-- begin #contact -->
         <div id="contact" class="content bg-silver-lighter" data-scrollview="true">
             <!-- begin container -->
@@ -286,7 +427,7 @@ $mysqli = initializeMysqlConnection();
                             <div class="form-group row m-b-15">
                                 <label class="col-form-label col-md-3 text-md-right">Message <span class="text-theme">*</span></label>
                                 <div class="col-md-9">
-                                    <textarea class="form-control" name="message" rows="10"></textarea>
+                                    <textarea class="form-control" name="message" rows="10" required="required"></textarea>
                                 </div>
                             </div>
                             <div class="form-group row m-b-15">
