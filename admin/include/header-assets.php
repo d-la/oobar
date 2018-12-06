82ac0767272aa394438b5de5b1e56af06cb06c2e
Daniel La <la.danieln@gmail.com>
2018-12-05 23:15:56 -0500

    Updated favicon for all admin pages

--- admin/include/header-assets.php
+++ admin/include/header-assets.php
@@ -1,6 +1,7 @@
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
 <meta content="" name="description" />
 <meta content="" name="author" />
+<link rel="shortcut icon" type="image/png" href="/img/the_one_favicon1.png"/>
 
 <!-- ================== BEGIN BASE CSS STYLE ================== -->
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
