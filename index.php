2adc985d153e13a94c32d10d50ba62aa48407370
Daniel La <la.danieln@gmail.com>
2018-12-06 23:49:53 -0500

    Gallery added to the front end

--- index.php
+++ index.php
@@ -214,6 +214,7 @@ $mysqli = initializeMysqlConnection();
                                     <span class="carousel__dot carousel__dot--small carousel__dot--color-black"></span> -->
                                 </div>
                             <?php
+                                $rs->close();
                             } else { ?>
                                 <p class="content-desc text-center">
                                     No upcoming events! Contact us if you'd like to book an event.
@@ -234,135 +235,57 @@ $mysqli = initializeMysqlConnection();
 			<!-- begin container -->
 			<div class="container" data-animation="true" data-animation-type="fadeInDown">
 				<h2 class="content-title">Gallery</h2>
-				<p class="content-desc">
-				</p>
+                <?php
+                $sqlQuery = 'SELECT id, name, description, path FROM gallery_images;';
+                $resultSet = $mysqli->query($sqlQuery);
+                $numRows = $resultSet->num_rows;
+
+                if ($numRows == 0){
+                ?>
+                <p class="content-desc text-center">
+                    No photos available!
+                </p>
+                <?php 
+                } else {
+                ?>
 				<!-- begin row -->
 				<div class="row row-space-10">
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-1.jpg" alt="Work 1" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Aliquam molestie</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-2.jpg" alt="Work 2" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Quisque at pulvinar lacus</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-3.jpg" alt="Work 3" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Vestibulum et erat ornare</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-4.jpg" alt="Work 4" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Sed vitae mollis magna</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
+                    <?php
+                    $count = 0;
+                    if ($resultSet->num_rows > 0){
+                        while ($row = $resultSet->fetch_assoc()){
+                            $title = $row['name'];
+                            $description = $row['description'];
+                            $path = $row['path'];
+                            ?>
+                            <!-- begin col-3 -->
+                            <div class="col-md-3 col-sm-6">
+                                <!-- begin work -->
+                                <div class="work" data-click="show-gallery-image" data-position="<?= $count; ?>"> 
+                                    <div class="image">
+                                        <a href="#"><img src="<?= $path ?>" alt="<?= $title ?>" /></a>
+                                    </div>
+                                    <div class="desc">
+                                        <span class="desc-title"><?= $title; ?></span>
+                                        <!-- <span class="desc-text">Lorem ipsum dolor sit amet</span> -->
+                                    </div>
+                                </div>
+                                <!-- end work -->
+                            </div>
+                            <!-- end col-3 -->
+
+                            <?php
+                            // Keep track of each photo's position
+                            $count++;
+                        }
+
+                        $resultSet->close();
+                    }
+                    ?>
 				</div>
-				<!-- end row -->
-				<!-- begin row -->
-				<div class="row row-space-10">
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-5.jpg" alt="Work 5" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Suspendisse at mattis odio</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-6.jpg" alt="Work 6" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Aliquam vitae commodo diam</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-7.jpg" alt="Work 7" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Phasellus eu vehicula lorem</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-					<!-- begin col-3 -->
-					<div class="col-md-3 col-sm-6">
-						<!-- begin work -->
-						<div class="work">
-							<div class="image">
-								<a href="#"><img src="../assets/img/work/work-img-8.jpg" alt="Work 8" /></a>
-							</div>
-							<div class="desc">
-								<span class="desc-title">Morbi bibendum pellentesque</span>
-								<span class="desc-text">Lorem ipsum dolor sit amet</span>
-							</div>
-						</div>
-						<!-- end work -->
-					</div>
-					<!-- end col-3 -->
-				</div>
+                <?php 
+                }
+                ?>
 				<!-- end row -->
 			</div>
 			<!-- end container -->
